bdba26d86195ac9d4a7b0a4f72cd1b05caf9ba56
xtreemes <52927886+Xtreemes@users.noreply.github.com>
2024-03-03 20:54:06 -0700

    Test commit

--- src/main/java/org/xtreemes/voicechat/client/VoiceChatClient.java
+++ src/main/java/org/xtreemes/voicechat/client/VoiceChatClient.java
@@ -2,12 +2,14 @@ package org.xtreemes.voicechat.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import com.jagrosh.discordipc.entities.pipe.Pipe;
 
 public class VoiceChatClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         //PipeManager.setClientID(910316364269494332L);
-        //PipeManager.initializePipe();
+        PipeManager.initializePipe();
+        Pipe pipe;
 
         ClientTickEvents.START_CLIENT_TICK.register(clientt -> {
             System.out.println("test");
